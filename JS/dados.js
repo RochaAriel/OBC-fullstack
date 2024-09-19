feb585210ba4cc825a959f5f8db3137158cd452a
teste <arielalves.alves@gmail.com>
2024-09-19 13:54:37 -0300

    Basic
    .

---
+++ JS/dados.js
@@ -0,0 +1,10 @@
+const numero = 1 ;
+var bigint = 33333333333333333333333333333333333333333333333333333n;
+const nome = "Ariel";
+
+" null == Valor vazio"
+"Undefined ==  nao definido "
+
+console.log(numero);
+console.log(bigint);
+console.log(nome);
